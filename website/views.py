f376ba8f1dacc2085164163f6fafa47db94e6c85
tolgakb <tolgakabaahmet@gmail.com>
2023-07-24 15:52:44 +0100

    Records are able to viewed individually at a separate page.

--- website/views.py
+++ website/views.py
@@ -50,3 +50,13 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
     
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You must be logged in to view that page')
+        return redirect('home')
